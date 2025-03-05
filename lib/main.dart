c2eac1e0fad108aaf0ff02da6072d7c81817144a
HIDExTM <hide.toku.mora.2004@gmail.com>
2025-03-05 12:55:42 -0500

    La ultima

--- lib/main.dart
+++ lib/main.dart
@@ -1,102 +1,27 @@
-import 'package:connectivity_plus/connectivity_plus.dart';
-import 'package:flutter/foundation.dart' show kIsWeb;
 import 'package:flutter/material.dart';
-import 'package:flutter_secure_storage/flutter_secure_storage.dart';
 import 'package:go_router/go_router.dart';
-import 'package:http/http.dart';
-import 'package:provider/provider.dart';
-import 'package:shared_preferences/shared_preferences.dart';
-import 'package:url_strategy/url_strategy.dart';
 
-import 'app/data/http/http.dart';
-import 'app/data/services/remote/internet_checker.dart';
-import 'app/domain/repositories/preferences_repository.dart';
-import 'app/inject_repositories.dart';
-import 'app/my_app.dart';
-import 'app/presentation/global/controllers/favorites/favorites_controller.dart';
-import 'app/presentation/global/controllers/favorites/state/favorites_state.dart';
-import 'app/presentation/global/controllers/session_controller.dart';
-import 'app/presentation/global/controllers/theme_controller.dart';
-import 'app/presentation/routes/routes.dart';
+import 'app/presentation/modules/sign_in/views/sign_in_view.dart';
 
-void main() async {
-  setPathUrlStrategy();
-  WidgetsFlutterBinding.ensureInitialized();
-
-  print('🔄 Iniciando configuración...');
-
-  final secureStorage = kIsWeb ? null : const FlutterSecureStorage();
-  final apiKey = kIsWeb
-      ? '5bed77d45c8c0c13c37c7800f7af6aa1'
-      : await secureStorage!.read(key: 'api_key') ??
-          '5bed77d45c8c0c13c37c7800f7af6aa1';
-
-  final http = Http(
-    client: Client(),
-    baseUrl: 'https://api.themoviedb.org/3',
-    apiKey: apiKey,
-  );
-
-  final connectivity = kIsWeb ? null : Connectivity();
-  final systemDarkMode = kIsWeb
-      ? false
-      : WidgetsBinding.instance.window.platformBrightness == Brightness.dark;
-
-  print('🔄 Inyectando dependencias...');
-  await injectRepositories(
-    systemDarkMode: systemDarkMode,
-    http: http,
-    languageCode: 'en', // Se puede cambiar según idioma
-    secureStorage: FlutterSecureStorage(),
-    preferences: await SharedPreferences.getInstance(),
-    connectivity: Connectivity(),
-    internetChecker: InternetChecked(),
-  );
-  print('🎉 injectRepositories finalizó correctamente.');
-
-  print('✅ Repositorios cargados. Ejecutando app...');
-  runApp(const Root(initialRoute: Routes.signIn));
+void main() {
+  runApp(const MyApp());
 }
 
-class Root extends StatelessWidget {
-  const Root({
-    super.key,
-    this.initialRoute,
-    this.overrideRoutes,
-  });
-
-  final String? initialRoute;
-  final List<GoRoute>? overrideRoutes;
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return MultiProvider(
-      providers: [
-        ChangeNotifierProvider<ThemeController>(
-          create: (context) {
-            final PreferencesRepository preferencesRepository =
-                Repositories.preferences;
-            return ThemeController(
-              preferencesRepository.darkMode,
-              preferencesRepository: preferencesRepository,
-            );
-          },
-        ),
-        ChangeNotifierProvider<SessionController>(
-          create: (context) => SessionController(
-            authenticationRepository: Repositories.authentication,
+    return MaterialApp.router(
+      debugShowCheckedModeBanner: false,
+      routerConfig: GoRouter(
+        initialLocation: '/sign-in',
+        routes: [
+          GoRoute(
+            path: '/sign-in',
+            builder: (context, state) => const SignInView(),
           ),
-        ),
-        ChangeNotifierProvider<FavoritesController>(
-          create: (context) => FavoritesController(
-            FavoritesState.loading(),
-            accountRepository: Repositories.account,
-          ),
-        ),
-      ],
-      child: MyApp(
-        initialRoute: initialRoute ?? Routes.signIn,
-        overrideRoutes: overrideRoutes,
+        ],
       ),
     );
   }
